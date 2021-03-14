2f1b347ea69da53c1145bebfa5288784f9426291
Serhii B <s_blyshchak@ukr.net>
2021-03-14 09:17:51 +0200

    added form

--- App.js
+++ App.js
@@ -1,18 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { AddTodo } from './src/AddTodo';
 import { Navbar } from './src/Navbar';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View >
       <Navbar title="ToDo App"/>
+      <View style={styles.container}>
+      <AddTodo />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    paddingHorizontal: 30,
+    paddingVertical: 30,
     
   }
 });
---
+++ src/AddTodo.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import {View, StyleSheet, TextInput, Button} from 'react-native'
+
+export const AddTodo = () => {
+    return  (
+        <View style={styles.block}>
+            <TextInput style={styles.input} />
+            <Button title="Додати"/>
+
+        </View>
+    )
+}
+
+const styles =StyleSheet.create({
+    block: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+    input: {
+        width: '70%',
+        padding: 10,
+        borderStyle: 'solid',
+        borderBottomWidth: 2,
+        borderBottomColor: '#3949ab'
+
+    }
+})
